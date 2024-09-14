babdc6ec06f8845115e6e4974872398c3e6e122a
anhkhoa1408 <akhoa981@gmail.com>
2024-09-15 02:00:28 +0700

    feat(footer): temporary add footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import Header from "./layouts/Header.vue";
-
+import Footer from "./layouts/Footer.vue";
 </script>
 
 <template>
   <Header />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/layouts/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="flex flex-col items-center justify-center">
+    <h1 class="text-3xl font-bold mb-5">Feel free to connect on social media</h1>
+    <p class="font-semibold">Developed by Nguyen Anh Khoa</p>
+  </footer>
+</template>
+
+<style lang="css" scoped>
+footer {
+  background: url("./../../public/images/background/wave.svg") no-repeat center;
+  background-size: cover;
+  height: 400px;
+  color: white;
+}
+</style>
