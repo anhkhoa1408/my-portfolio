b077705c60365a17ddecb90923a8d7ff338402f7
anhkhoa1408 <akhoa981@gmail.com>
2024-09-16 00:57:43 +0700

    fix(responsive): update responsive

--- src/components/home/Education.vue
+++ src/components/home/Education.vue
@@ -2,14 +2,12 @@
 
 <template>
   <div class="flex flex-col bg-glass px-6 py-4 gap-3 rounded-md">
-    <div class="flex items-center justify-start gap-3">
+    <div class="flex items-start lg:items-center justify-start gap-2 lg:gap-3 flex-col lg:flex-row">
       <p class="font-semibold">Bachelor of Computer Science</p>
-      <div class="size-2 bg-blue-500 rounded-full"></div>
-      <p class="gradient-text">Ho Chi Minh University of Technology</p>
+      <div class="lg:block hidden size-2 bg-blue-500 rounded-full"></div>
+      <p class="gradient-text text-sm lg:text-base">Ho Chi Minh City University of Technology (HCMUT)</p>
     </div>
-    <div class="flex items-center gap-4 text-sm">
-      <p>GPA: <strong>7.07</strong> <i>(7.07/10)</i></p>
-    </div>
+    <p class="text-sm">GPA: <strong>7.07</strong> <i>(7.07/10)</i></p>
   </div>
 </template>
 
--- src/components/home/Experiences.vue
+++ src/components/home/Experiences.vue
@@ -2,11 +2,14 @@
 
 <template>
   <div class="flex flex-col bg-glass px-6 py-4 gap-3 rounded-md">
-    <div class="flex items-center justify-start gap-3">
-      <p class="font-semibold">Frontend Developer</p>
-      <div class="size-2 bg-blue-500 rounded-full"></div>
-      <p class="gradient-text">Vietnam Blockchain Corporation</p>
-      <div class="bg-primary px-2 py-1 text-xs">Full-time</div>
+    <div class="flex items-start lg:items-center justify-start gap-2 lg:gap-3 flex-col lg:flex-row">
+      <div class="flex items-center gap-2">
+        <p class="font-semibold">Frontend Developer</p>
+        <div class="size-2 bg-blue-500 rounded-full"></div>
+        <div class="block lg:hidden bg-primary px-2 py-1 text-xs">Full-time</div>
+      </div>
+      <p class="gradient-text text-sm lg:text-base">Vietnam Blockchain Corporation</p>
+      <div class="hidden lg:block bg-primary px-2 py-1 text-xs">Full-time</div>
     </div>
     <div class="flex items-center gap-4 text-sm">
       <p>April 2021</p>
