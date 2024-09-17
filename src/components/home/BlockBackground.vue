bcdd0cd9baf12b597a9319e3a530b3982c98ae44
anhkhoa1408 <akhoa981@gmail.com>
2024-09-17 11:08:24 +0700

    fix(block): change size

--- src/components/home/BlockBackground.vue
+++ src/components/home/BlockBackground.vue
@@ -1,42 +1,47 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, onUnmounted } from "vue";
+
+const mouseEnterBlockHandler = (e: Event) => {
+  const target = e.target as HTMLDivElement;
+  if (target?.closest(".block")) {
+    target.classList.toggle("fade");
+  }
+};
 
 onMounted(() => {
   const blockWrapEle = document.getElementById("block-wrap");
+  blockWrapEle?.addEventListener("mouseover", mouseEnterBlockHandler);
+});
 
-  if (!blockWrapEle) return;
-
-  blockWrapEle.addEventListener("mouseover", (e) => {
-    const target = e.target as HTMLDivElement;
-    if (target?.closest(".block")) {
-      target.classList.toggle("fade");
-    }
-  });
+onUnmounted(() => {
+  const blockWrapEle = document.getElementById("block-wrap");
+  blockWrapEle?.removeEventListener("mouseover", mouseEnterBlockHandler);
 });
 </script>
 
 <template>
   <div
     id="block-wrap"
-    class="items-center flex-wrap content-center overflow-hidden hidden lg:flex absolute left-0 right-0 top-0 h-[600px] z-0"
+    class="items-center flex-wrap content-center overflow-hidden hidden lg:flex absolute left-0 right-0 top-0 z-0"
   >
-    <div v-for="i in 100" :key="i" class="max-w-[7%] basis-[7%] h-[105px] shrink-0 block"></div>
+    <div v-for="i in 100" :key="i" class="max-w-[5%] basis-[5%] h-[70px] shrink-0 block m-1"></div>
   </div>
 </template>
 
 <style lang="css" scoped>
 .fade {
-  animation: Fade 0.75s ease-in-out;
+  animation: Fade 1.5s ease-in-out;
 }
 
 @keyframes Fade {
-  from {
+  0%,
+  20% {
+    opacity: 0.5;
+    @apply bg-secondary-50;
+  }
+  100% {
     opacity: 0;
     background-color: transparent;
-  }
-  to {
-    opacity: 0.3;
-    @apply bg-slate-50;
   }
 }
 </style>
