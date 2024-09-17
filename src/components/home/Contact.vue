3cd9ef72e570191a922439131683027d0b4622a1
anhkhoa1408 <akhoa981@gmail.com>
2024-09-17 10:50:40 +0700

    feat(contact): temporary add form

--- src/components/home/Contact.vue
+++ src/components/home/Contact.vue
@@ -1,18 +1,54 @@
 <script setup lang="ts">
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Label } from "../ui/label";
 import { ArrowRightIcon } from "@heroicons/vue/16/solid";
+import { ref, watch } from "vue";
+import { Label } from "../ui/label";
+import { appConfigs } from "@/configs/app";
+
+const name = ref("");
+const email = ref("");
+const description = ref("");
+
+const valid = ref(false);
+
+watch([name, email, description], () => {
+  const formEle = document.querySelector("#contact-form") as HTMLFormElement;
+  const isInputValid = Array.from(formEle.querySelectorAll("input")).every((input) => input.checkValidity());
+  const isTextareaValid = Array.from(formEle.querySelectorAll("textarea")).every((input) => input.checkValidity());
+  valid.value = isInputValid && isTextareaValid;
+});
+
+const handleSubmit = async () => {
+  const data = {
+    name: name.value,
+    from: email.value,
+    to: appConfigs.receiverEmail,
+    description: description.value,
+  };
+
+  await fetch(appConfigs.mailerHost.concat("/api/v1/email/send") || "", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": appConfigs.mailerApiKey,
+    },
+    body: JSON.stringify(data),
+  });
+};
 </script>
 
 <template>
   <section>
     <div class="container mx-auto py-10 md:py-16 lg:py-32 flex flex-col">
       <h1 class="font-bold text-3xl xl:text-5xl mb-10 self-center text-center gradient-text">Contact</h1>
-      <div class="w-full md:w-8/12 lg:w-6/12 mx-auto flex flex-col">
+      <form id="contact-form" @submit.prevent="handleSubmit" class="w-full md:w-8/12 lg:w-6/12 mx-auto flex flex-col">
         <div class="grid w-full items-center gap-2">
           <Label for="email" class="mb-2">Name</Label>
           <Input
+            required
+            type="text"
+            v-model="name"
             placeholder="Your Name"
             class="bg-transparent border-2 border-input placeholder:text-input-placeholder mb-8 lg:mb-10 text-base h-[50px]"
           />
@@ -20,6 +56,9 @@ import { ArrowRightIcon } from "@heroicons/vue/16/solid";
         <div class="grid w-full items-center gap-2">
           <Label for="email" class="mb-2">Email</Label>
           <Input
+            required
+            type="email"
+            v-model="email"
             placeholder="Your Email"
             class="bg-transparent border-2 border-input placeholder:text-input-placeholder mb-8 lg:mb-10 text-base h-[50px]"
           />
@@ -27,16 +66,23 @@ import { ArrowRightIcon } from "@heroicons/vue/16/solid";
         <div class="grid w-full items-center gap-2 mb-6">
           <Label for="email" class="mb-2">Description</Label>
           <Textarea
+            required
+            type="text"
+            v-model="description"
             rows="6"
             placeholder="Describe something"
             class="bg-transparent border-2 border-input placeholder:text-input-placeholder text-base"
           />
         </div>
-        <button class="btn btn-primary w-[120px] self-center flex items-center submit-btn overflow-hidden">
+        <button
+          :disabled="!valid"
+          type="submit"
+          class="btn btn-primary w-[120px] self-center flex items-center submit-btn overflow-hidden"
+        >
           <span>Submit</span>
           <span><ArrowRightIcon class="size-4" /></span>
         </button>
-      </div>
+      </form>
     </div>
   </section>
 </template>
@@ -60,7 +106,7 @@ import { ArrowRightIcon } from "@heroicons/vue/16/solid";
     }
   }
 
-  &:hover {
+  &:not(:disabled):hover {
     span {
       &:first-child {
         top: 50%;
