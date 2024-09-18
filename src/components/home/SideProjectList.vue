e83fd83c266298fa7336e212e33e68bf5d491797
anhkhoa1408 <akhoa981@gmail.com>
2024-09-18 20:25:29 +0700

    style(side-project): update responsive

--- src/components/home/SideProjectList.vue
+++ src/components/home/SideProjectList.vue
@@ -8,14 +8,16 @@ gsap.registerPlugin(ScrollTrigger);
 
 const projects = [
   {
+    name: "My Portfolio",
+    imageUrl: "/images/project/my-port.png",
+    url: "object-left-top",
+    class: "object-left-top",
+  },
+  {
     name: "Designer Portfolio",
     imageUrl: "/images/project/pnhi-port.png",
     url: "",
-  },
-  {
-    name: "My Portfolio",
-    imageUrl: "/images/project/my-port.png",
-    url: "",
+    class: "object-left-top",
   },
   {
     name: "E-store Flower",
@@ -36,29 +38,34 @@ const projects = [
 ];
 
 onMounted(() => {
-  const projects = gsap.utils.toArray("#projects .side-project");
+  const matchMedia = gsap.matchMedia();
 
-  gsap.to(projects, {
-    xPercent: -100 * (projects.length - 2),
-    scrollTrigger: {
-      trigger: "#projects",
-      pin: true,
-      start: "top top",
-      end: "bottom top",
-      scrub: true,
-    },
+  matchMedia.add("(min-width: 1024px)", () => {
+    const projects = gsap.utils.toArray("#side-projects .side-project");
+    gsap.to(projects, {
+      xPercent: -100 * (projects.length - 1),
+      padding: 0,
+      scrollTrigger: {
+        trigger: "#side-projects",
+        pin: true,
+        pinnedContainer: "#side-projects",
+        start: "top 200px",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
   });
 });
 </script>
 
 <template>
-  <id id="side-projects" class="flex overflow-x-hidden flex-nowrap gap-4 lg:gap-10 px-5 lg:px-10">
+  <id id="side-projects" class="flex flex-nowrap gap-4 lg:gap-10 px-5 lg:px-10">
     <template v-for="(project, index) in projects" :key="index">
       <div
-        class="flex-none w-[320px] h-[250px] lg:h-[300px] lg:w-[450px] bg-white rounded-lg side-project transform-gpu overflow-hidden"
+        class="flex-none w-[80vw] h-[300px] md:h-[300px] md:w-[450px] lg:h-[400px] lg:w-[500px] bg-white rounded-lg side-project transform-gpu overflow-hidden"
       >
         <img
-          :class="['w-full h-full object-cover object-left-top rounded-lg overflow-hidden', project.class]"
+          :class="['w-full h-full object-cover rounded-lg overflow-hidden', project.class]"
           :src="project.imageUrl"
         />
       </div>
@@ -70,5 +77,22 @@ onMounted(() => {
 .cursor-detail {
   @apply bg-primary text-white p-2 fixed z-50;
   transition: opacity 0.2s ease, transform 0.2s ease;
+}
+
+@media (hover: hover) {
+  #side-projects {
+    overflow-x: hidden;
+  }
+}
+
+@media (hover: none) {
+  #side-projects {
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+
+    & .side-project {
+      scroll-snap-align: center;
+    }
+  }
 }
 </style>
