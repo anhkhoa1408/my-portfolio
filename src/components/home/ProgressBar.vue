fb01eb4bb1aa80f2aad1fe3b8bd2e5b522c8ae19
anhkhoa1408 <akhoa981@gmail.com>
2024-09-19 23:17:20 +0700

    feat(progress): add progress bar

---
+++ src/components/home/ProgressBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, useTemplateRef } from "vue";
+
+const progressRef = useTemplateRef<HTMLDivElement>("progress");
+
+const updateProgressWidth = () => {
+  if (!progressRef?.value?.style) return;
+
+  progressRef.value.style.width = `${
+    (window.scrollY * 100) / (document.documentElement.scrollHeight - window.innerHeight)
+  }%`;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", updateProgressWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", updateProgressWidth);
+});
+</script>
+
+<template>
+  <div ref="progress" class="fixed z-[1000] top-0 left-0 h-[5px] bg-gradient-main"></div>
+</template>
+
+<style lang="css" scoped></style>
